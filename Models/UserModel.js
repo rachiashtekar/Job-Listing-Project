07dbb3884e8db91363b67f6ef4c6c295e0f4c0ef
rachi <rachiashtekar345@gmail.com>
2024-05-06 21:32:39 +0530

    first commit

---
+++ Models/UserModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("user", userSchema);
---
+++ Routes/AuthRoute.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const
+{registerController ,
+  loginController,
+
+} = require("../Controllers/AuthControllers")
+
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+
+
+module.exports=router;
